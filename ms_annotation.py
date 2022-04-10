4e09eb8b62342c1428d6f3606412303b00ed37e7
Anthony Beckett <anthonyjbeckett@protonmail.com>
2022-04-10 19:24:08 +0930

    refactored minorly

--- ms_annotation.py
+++ ms_annotation.py
@@ -5,17 +5,19 @@ import formula as mf
 import frag_tree as ft
 import netcdf
 import networkx as nx
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 
 def file_loop():
     while True:
         try:
-            a = input("Please enter the name of your data file (case sensitive), making sure to include file extensions (e.g MyFile.cdf): ")
+            a = str(input("Please enter the name of your data file (case sensitive), making sure to include file extensions (e.g MyFile.cdf): "))
             ### need to add ways to handle other file types ###
-            data = nc.Dataset(os.path.dirname(sys.argv[0]) + "\\" + a)
+            if not os.path.isfile(a):
+                raise(FileNotFoundError)
+            data = nc.Dataset(a)
             return data
-    
+
         except FileNotFoundError:
             print("File not found! Make sure you have included the file extension in the name and the data file is in this folder! \n")
 
@@ -48,11 +50,11 @@ def plot_mass_spectrum(spectrum):
     a = [d[i] if i in d else 0 for i in range(max_mass)]
     plt.bar(mz, a, color ='blue',
         width = 0.4)
-    
+
     for p in spectrum:
         plt.annotate(p[0], # this is the text
                 (int(round(p[0],0)), p[1]),
-                fontsize=8, 
+                fontsize=8,
                 textcoords="offset points", # how to position the text
                 xytext=(0,2), # distance from text to points (x,y)
                 ha='center') # horizontal alignment can be left, right or center
@@ -70,13 +72,13 @@ def plot_annotated_mass_spectrum(spectrum, graph):
     a = [d[i] if i in d else 0 for i in range(max_mass)]
     plt.bar(mz, a, color ='blue',
         width = 0.4)
-    
+
     fs = ft.return_formulae(ft.extract_mass(spectrum),graph,to_dict=True)
     for p in spectrum:
         if int(round(p[0],0)) in fs:
             plt.annotate(fs[int(round(p[0],0))], # this is the text
                 (int(round(p[0],0)), p[1]),
-                fontsize=8, 
+                fontsize=8,
                 textcoords="offset points", # how to position the text
                 xytext=(0,2), # distance from text to points (x,y)
                 ha='center') # horizontal alignment can be left, right or center
@@ -93,7 +95,7 @@ def MS_loop(maxscan):
             print("The maximum scan number is {}".format(maxscan))
             n = input("Please enter a scan number from which you want to retrieve your mass spectrum: ")
             n = int(n.strip())
-            try: 
+            try:
                 print("Scan number received! Plotting mass spectrum..")
                 ms = netcdf.mass_spectra(n, data, intensity_cutoff=0.5,base_norm=True)
                 plot_mass_spectrum(ms)
@@ -105,8 +107,8 @@ def MS_loop(maxscan):
                 print("Error, there doesn't seem to be a peak here!")
         except ValueError:
             print("Error, need to input integer!")
-        
-        
+
+
 
 def formula_loop(graphs, spec):
     while True:
@@ -114,10 +116,10 @@ def formula_loop(graphs, spec):
             print("There are {} sets of annotations possible (1 - {})".format(len(graphs),len(graphs)))
             num = int(input("Please select which set you want to view: "))
             l = ft.return_formulae(spec, graphs[num -1])
-            
+
             for formula in l:
                 print("Mass: {}                  Formula: {}".format(formula[1], formula[0]))
-            
+
             answer = input("Do you want to choose this set of annotations? (y/n): ")
             if answer == "y" or answer == "Y" or answer == "yes" or answer == "YES":
                 return num -1
